07b336c4852a42a209a125f97cde24f7a672e349
dbkimball <dkimball311@msn.com>
2023-05-15 21:27:55 -0700

    updates

--- chamber/scripts/currentTime
+++
@@ -1,5 +0,0 @@
-let date1 = document.getElementById("currentTime");
-
-const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en", {dateStyle: "full"}).format(now);
-date1.innerHTML = fulldate;
--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -4,3 +4,13 @@ document.querySelector('#year').textContent = currentYear;
 let text = document.lastModified;
 const date = new Date(document.lastModified);
 document.getElementById("currentdate").innerHTML = text;
+
+let date1 = document.getElementById("currentTime");
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en", {dateStyle: "full"}).format(now);
+date1.innerHTML = fulldate;
+
+function toggleMenu() {
+    document.getElementById('primaryNav')[0].classList.toggle('responsive');
+}
