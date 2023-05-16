66b049e1b2483cefeb531d7a16e498f55a3b408e
dbkimball <dkimball311@msn.com>
2023-05-15 22:07:01 -0700

    updates

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -11,6 +11,27 @@ const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en", {dateStyle: "full"}).format(now);
 date1.innerHTML = fulldate;
 
-function toggleMenu() {
-    document.getElementById('primaryNav')[0].classList.toggle('responsive');
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
 }
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
+
+
+/*function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu;
+
+/*const hamburgerBtn = document.querySelector('#hamburgerBtn');
+const primaryNav = document.querySelector('primaryNav');
+hamburgerBtn.addEventListener('click', () => {
+    primaryNav.classList.toggle('responsive');
+});
+
+/*function toggleMenu() {
+    document.getElementById('primaryNav')[0].classList.toggle('responsive');
+}*/
