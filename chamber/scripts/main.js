2badbe9b020852d432e8adaeda9c898b6239b84c
dbkimball <dkimball311@msn.com>
2023-05-18 15:38:10 -0700

    date updates

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -5,10 +5,15 @@ let text = document.lastModified;
 const date = new Date(document.lastModified);
 document.getElementById("currentdatetime").innerHTML = text;
 
-let date1 = document.getElementById("currentDate");
-const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", {dateStyle: "full"}).format(now);
-date1.innerHTML = fulldate;
+const today = new Date();
+
+      const dayOfWeek = today.toLocaleDateString('en-US', { weekday: 'long' });
+      const dayOfMonth = today.getDate();
+      const month = today.toLocaleDateString('en-US', { month: 'long' });
+      const year = today.getFullYear();
+
+      const dateElement = document.getElementById('currentDate');
+      dateElement.textContent = `${dayOfWeek}, ${dayOfMonth} ${month} ${year}`;
 
 function toggleMenu(){
     document.getElementById("primaryNav").classList.toggle("open");
@@ -16,3 +21,7 @@ function toggleMenu(){
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
 
+// let date1 = document.getElementById("currentDate");
+// const now = new Date();
+// const fulldate = new Intl.DateTimeFormat("en-US", {dateStyle: "full"}).format(now);
+// date1.innerHTML = fulldate;
