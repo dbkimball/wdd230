2f04dba7d716f7334b7a9293e177d3c2b5dc3da5
dbkimball <dkimball311@msn.com>
2023-05-17 19:53:46 -0700

    updates chamber

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -3,9 +3,9 @@ document.querySelector('#year').textContent = currentYear;
 
 let text = document.lastModified;
 const date = new Date(document.lastModified);
-document.getElementById("currentdate").innerHTML = text;
+document.getElementById("currentdatetime").innerHTML = text;
 
-let date1 = document.getElementById("currentTime");
+let date1 = document.getElementById("currentDate");
 
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en", {dateStyle: "full"}).format(now);
