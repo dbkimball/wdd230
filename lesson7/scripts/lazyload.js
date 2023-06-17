0c26d5fa7fb3f5fff7393521c99ed5d443f7221a
dbkimball <dkimball311@msn.com>
2023-06-16 18:33:01 -0700

    updates

--- lesson7/scripts/lazyload.js
+++ lesson7/scripts/lazyload.js
@@ -29,27 +29,3 @@ if('IntersectionObserver' in window) {
 }
 
 
-
- 
-// function preLoadImage(img) {
-//     const src = img.getAttribute("data-src");
-//     if(!src){
-//         return;
-//     }
-//     img.src = src;
-// };
-
-// const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-//    entries.forEach(entry => {
-//     if(!entry.isIntersecting){
-//         return;
-//     } else {
-//         preLoadImage(entry.target);
-//         imgObserver.unobserve(entry.target);
-//     }
-//    }) 
-// }, imgOptions);
-
-// images.forEach(image => {
-//     imgObserver.observe(image);
-// });
