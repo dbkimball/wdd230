5d7743f3001fb070d8a138ccf341dc9347355aa7
dbkimball <dkimball311@msn.com>
2023-06-16 19:47:23 -0700

    updates

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -34,3 +34,50 @@ x.onclick = toggleMenu;
 if (dayValue < 3 && dayValue > 0){
     document.getElementById("banner").className = "visible";
 }
+
+
+//discover page lazy load 
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {image.removeAttribute('data-src');};
+};
+
+if('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+           items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+           });
+        }, imgOptions);
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img)=> {
+        loadImages(img);
+    });
+}
+
+//Days since last visit
+const visitDate = new Date();
+localStorage.setItem('lastVisit', visitDate);
+
+//retrieve the visit date from localStorage
+const lastVisit = localStorage.getItem('lastVisit');
+
+//calculate the days between visits
+const currentDate = new Date();
+const daysBetweenVisits = Math.floor((currentDate - new Date(lastVisit))/ (1000 *60 *60 *24));
+
+//display the number of days between visits on the page 
+document.getElementById('daysVisit').textContent = daysBetweenVisits;
+
