72461735f615583487cc2d555a9ed7701f91a3ce
dbkimball <dkimball311@msn.com>
2023-06-16 17:58:24 -0700

    updates week 6

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,13 +1,20 @@
 //Get the two input values of temp and wind speed
-let temperature = document.getElementById('temperature');
-const temp = 50;
-temp.innerHTMl = '$(temp) °F';
+let temperature = document.getElementById('temperature').innerText;
+let temp = Number(temperature);
 
-let windSpeed = document.getElementById('windSpeed');
-const speed = 5;
-windSpeed.innertHTML = '$(speed) mph';
-console.log(speed);
+let windSpeed = document.getElementById('windSpeed').innerText;
+let speed = Number(windSpeed);
+console.log(typeof(windSpeed));
 
-//check to make sure they meet the specification limits  (<=50°F and >3.0mph) allowed to officially calculate the wind chill, and
+//check to make sure they meet the specification limits  (<=50°F and >3.0mph) allowed to officially calculate the wind chill, and either calculate and display the wind chill factor value or display "N/A" (not applicable) if the input values did not meet the requirements.
+if (temperature <=50 && windSpeed >3){
+    let windChill = 35.74 + 0.6215*temp - 35.75*(speed**0.16) + 0.4275*(temp*speed**0.16);
+    console.log(windChill);
+    document.getElementById('windChill').innerHTML=Math.round(windChill);
+} 
+else{
+    document.getElementById('windChill').innerHTML ='N/A';
+}
 
-//either calculate and display the wind chill factor value or display "N/A" (not applicable) if the input values did not meet the requirements. The formula to calculate the wind chill factor is  =35.74+0.6215 −35.75 0.16+0.4275  0.16, where f is the wind chill factor in Fahrenheit, t is the air average temperature in Fahrenheit, and s is the wind speed in miles per hour.
+
+ 
