907f29147f3e041391c855c19c3496653b6835cd
dbkimball <dkimball311@msn.com>
2023-07-20 00:05:37 -0700

    update

--- bountifulfoods/scripts/weather.js
+++ bountifulfoods/scripts/weather.js
@@ -2,8 +2,10 @@
 const currentTemp = document.querySelector('#temperature');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
+const humidityElement = document.querySelector('#humidity');
 
 const url = "https://api.openweathermap.org/data/2.5/weather?q=Carlsbad&units=imperial&appid=93b19be343e708d2c8cd63d952b6bee8";
+
 
 async function apiFetch() {
     try {
@@ -33,9 +35,51 @@ async function displayResults(weatherData){
     captionDesc.textContent = desc;
 
     document.getElementById("temperature").innerHTML = temp;
+    const humidityValue = weatherData.main.humidity;
+    humidityElement.textContent = humidityValue;
     
 }
 
 apiFetch();
 
- 
+// const forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=Carlsbad&units=imperial&appid=93b19be343e708d2c8cd63d952b6bee8";
+
+// async function fetchWeatherForecast() {
+//   try {
+//     const response = await fetch(apiUrl);
+//     const data = await response.json();
+
+//     const forecastDiv = document.getElementById('forecast');
+//     forecastDiv.innerHTML = '<h2>3-Day Forecast:</h2>';
+
+//     // Loop through the list of forecast data (each data represents a 3-hour interval for the next 3 days)
+//     for (let i = 0; i < data.list.length; i++) {
+//       const forecastData = data.list[i];
+
+//       // Extract the necessary information for display (e.g., date, temperature, humidity, condition)
+//       const date = new Date(forecastData.dt * 1000); // Convert Unix timestamp to JavaScript Date object
+//       const temperature = forecastData.main.temp;
+//       const humidity = forecastData.main.humidity;
+//       const condition = forecastData.weather[0].description;
+
+//       // Create a new forecast card element
+//       const forecastCard = document.createElement('div');
+//       forecastCard.classList.add('forecast-card');
+//       forecastCard.innerHTML = `
+//         <p>Date: ${date.toLocaleDateString()}</p>
+//         <p>Temperature: ${temperature} °C</p>
+//         <p>Humidity: ${humidity}%</p>
+//         <p>Condition: ${condition}</p>
+//       `;
+
+//       forecastDiv.appendChild(forecastCard);
+//     }
+//   } catch (error) {
+//     console.error('Error fetching weather forecast:', error);
+//   }
+// }
+
+// // Call the function to fetch and display the weather forecast when the page is loaded
+// window.addEventListener('DOMContentLoaded', fetchWeatherForecast);
+
+
